3d5ef94074df438a0bc53ba43a7837d6c14080e3
Radek Podgorny <radek@podgorny.cz>
2017-04-19 10:02:32 +0200

    more formatting

--- atxmon/cfg.py
+++ atxmon/cfg.py
@@ -11,21 +11,19 @@ class Config:
 	def read_from_ini(self, fn):
 		ini = ConfigParser()
 		ini.read(fn)
-
 		self.host = ini.get('General', 'Host', fallback=self.host)
 		#self.interval = ini.getint('General', 'Interval', fallback=self.interval)
 		self.url = ini.get('General', 'Url', fallback=self.url)
 
 	def check(self):
-		if not self.url: return 'no url!'
+		if not self.url:
+			return 'no url!'
 
 	# TODO: move this to some common module
 	def __str__(self):
 		l = []
-
 		for k, v in vars(self).items():
 			l.append('%s=\'%s\'' % (k, v))
-
 		return ', '.join(l)
 
 
